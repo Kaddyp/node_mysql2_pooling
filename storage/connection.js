2588203b7e860f5d961f12a5255ab0ae6fb4451b
Nitin Pawar <nitin.pawar@yondrgroup.com>
2024-06-27 17:18:32 +0100

    Setup .env files dor different envirnment.

--- storage/connection.js
+++ storage/connection.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
+require('dotenv').config()
 
 const access = {
-  user: 'root',
-  password: 'Test123',
-  host: 'localhost',
-  port: 3307,
-  database: 'userdb',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.SERVER_PORT || '3307',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit : 10,
 };
 
